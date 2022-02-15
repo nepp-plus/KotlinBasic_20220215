054824feb5539283a21dabc52e6f9418b96bfa64
nepp.plus <nepp.plus@gmail.com>
2022-02-15 11:43:21 +0900

    else if도 추가

--- app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
@@ -56,7 +56,7 @@ class MainActivity : AppCompatActivity() {
 
 //            조건문 연습
 
-            val userAge = 30
+            val userAge = 16
 
 //            사용자의 나이가 20살 이상? 맞으면 성인입니다. 토스트로
 
@@ -66,9 +66,19 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "성인입니다.", Toast.LENGTH_SHORT).show()
 
             }
+            else if (userAge >= 17) {
+                Toast.makeText(this, "고등학생입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else if (userAge >= 14) {
+//                17살 미만, 14살 이상.
+                Toast.makeText(this, "중학생입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else if (userAge >= 8) {
+                Toast.makeText(this, "초등학생입니다.", Toast.LENGTH_SHORT).show()
+            }
             else {
-//                위의 질문이 틀렸을때만 실행할 내용
-                Toast.makeText(this, "미성년자입니다.", Toast.LENGTH_SHORT).show()
+//                위의 모든 질문이 다 틀렸을때만 실행할 내용
+                Toast.makeText(this, "미취학 아동 입니다.", Toast.LENGTH_SHORT).show()
             }
 
 
