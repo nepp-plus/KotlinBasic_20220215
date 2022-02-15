e994039fe7efa5ff3c58bf285032055153ae23fd
nepp.plus <nepp.plus@gmail.com>
2022-02-15 11:12:47 +0900

    변수 기본 + val/var 차이 체험

--- app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
@@ -34,7 +34,23 @@ class MainActivity : AppCompatActivity() {
 
 //            변수에 대한 문법 연습용 코드
 
+//            본인의 이름을 변수에 담아보자. => 토스트로 띄우자.
 
+            var myName :  String
+            myName = "김강사"
+            myName = "조경진"
+
+            val friendName: String
+            friendName = "이선생"
+//            friendName = "김선생"     val에는 새 값 교체 불가.
+
+            Toast.makeText(this, friendName, Toast.LENGTH_SHORT).show()
+
+            var myBirthYear: Int
+            myBirthYear = 1988
+
+            var myHeight : Double
+            myHeight = 180.7
 
 
         }
