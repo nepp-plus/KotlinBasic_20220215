5efa24e83cbc523149eaf94e4d772eb3ac51adda
nepp.plus <nepp.plus@gmail.com>
2022-02-15 10:07:11 +0900

    버튼이 눌리면 > 로그 찍어보기

--- app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.kotlinbasic_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,6 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnLog.setOnClickListener {
+
+            Log.d("메인화면", "로그 버튼 눌림")
+            
+        }
 
     }
 
